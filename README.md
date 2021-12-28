86351cf8da44835a7befc3988a0a738df855e47d
Min <min@infcof.com>
2021-12-28 14:59:53 +0000

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# TI MSP430: development platform for [PlatformIO](http://platformio.org)
+
+The MSP432 is a mixed-signal microcontroller family from Texas Instruments. It is based on a 32-bit ARM Cortex-M4F CPU, and extends their 16-bit MSP430 line, with a larger address space for code and data, and faster integer and floating point calculation than the MSP430. Like the MSP430, it has a number of built-in peripheral devices, and is designed for low power requirements
+* [Datasheet](https://academics.uccs.edu/mlaubhan/common/MSP432/slau597b.pdf) (MSP‑EXP432P401R LaunchPad™ Development Kit)
+* [Datasheet](https://www.ti.com/lit/ds/slas826e/slas826e.pdf) (MSP432P401R microcontroller)
+
+# Usage
+
+1. [Install PlatformIO](http://platformio.org)
+2. Create PlatformIO project and configure a platform option in [platformio.ini](http://docs.platformio.org/page/projectconf.html) file:
+
+## Stable version
+
+```ini
+[env:stable]
+platform = timsp432
+board = ...
+...
+```
+
+## Development version
+
+```ini
+[env:development]
+platform = https://git.infcof.com/UCL/platform-timsp432.git
+board = ...
+...
+```
+
---
+++ builder/main.py
@@ -0,0 +1,133 @@
+import sys
+from os.path import join
+
+from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default, DefaultEnvironment)
+
+env = DefaultEnvironment()
+
+env.Replace(
+    AR="arm-none-eabi-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    GDB="arm-none-eabi-gdb",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rc"],
+
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.text.align|\.ARM.exidx)\s+(\d+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGSUFFIX=".elf"
+)
+
+
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-Os",
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-mmcu=$BOARD_MCU"
+    ],
+
+    CXXFLAGS=[
+        "-fno-exceptions",
+        "-fno-threadsafe-statics"
+    ],
+
+    CPPDEFINES=[
+        ("F_CPU", "$BOARD_F_CPU")
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-mmcu=$BOARD_MCU",
+        "-Wl,-gc-sections,-u,main"
+    ],
+
+    LIBS=["m"],
+
+    BUILDERS=dict(
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
+)
+
+# Allow user to override via pre:script
+if env.get("PROGNAME", "program") == "program":
+    env.Replace(PROGNAME="firmware")
+
+if "energia" in env.get("PIOFRAMEWORK", []):
+    sys.stderr.write(
+        "WARNING!!! Using of `framework = energia` in `platformio.ini` is "
+        "deprecated. Please replace with `framework = arduino`.\n")
+    env.Replace(PIOFRAMEWORK=["arduino"])
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.hex")
+else:
+    target_elf = env.BuildProgram()
+    target_firm = env.ElfToHex(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload firmware
+#
+
+upload_target = target_firm
+if env.subst("$UPLOAD_PROTOCOL") == "dslite":
+    env.Replace(
+        UPLOADER=join(env.PioPlatform().get_package_dir(
+            "tool-dslite") or "", "DebugServer", "bin", "DSLite"),
+        UPLOADERFLAGS=[
+            "load", "-c",
+            join(env.PioPlatform().get_package_dir("tool-dslite") or "",
+                 "%s.ccxml" % env.BoardConfig().get("build.variant")), "-f"
+        ],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES"
+    )
+    upload_target = target_elf
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+target_upload = env.Alias("upload", upload_target,
+                          env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE"))
+AlwaysBuild(target_upload)
+
+#
+# Default targets
+#
+
+Default([target_buildprog, target_size])
